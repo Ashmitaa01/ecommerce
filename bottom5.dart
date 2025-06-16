474e158e4a29f898cb3ebc8662a67c9fc4e775a4
Ashmitaa01 <140472804+Ashmitaa01@users.noreply.github.com>
2025-06-16 01:46:19 -0700

    Add files via upload

---
+++ bottom5.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Bottom5page extends StatefulWidget {
+  const Bottom5page({super.key});
+
+  @override
+  State<Bottom5page> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<Bottom5page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
